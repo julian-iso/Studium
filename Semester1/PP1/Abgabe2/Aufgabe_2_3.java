05629594fc9be4c3e41cc18e26fcf3e293314807
julian-iso <julian@Holl-iT-JM-Pro7.localdomain>
2022-09-23 13:48:11 +0200

    save

--- Semester1/PP1/Abgabe2/Aufgabe_2_3.java
+++ Semester1/PP1/Abgabe2/Aufgabe_2_3.java
@@ -1,7 +1,7 @@
 public class Aufgabe_2_3{
     // Aufgabenteil (a)
     // 3 4 6 9 13 18 24 ...
-    // Schema: an = an + n
+    // Schema: an = a(n-1) + (n-1)
     public static void folge_a() {
     	int n = 15;
     	int a = 3;
@@ -14,12 +14,12 @@ public class Aufgabe_2_3{
 
     // Aufgabenteil (b)
     // 4 5 3 6 7 5 8 9 ...
-    // Schema: 
+    // Schema:  
     public static void folge_b() {
     	int n = 15;
     	int a = 4;
     	int state = 1;
-    	for (int i = 0; i <= n; i++) {
+    	for (int i = 0; i <= n; i++) { 
     		System.out.print(a + " ");
     		switch (state) {
     		case 1:
@@ -42,28 +42,13 @@ public class Aufgabe_2_3{
     
     // Aufgabenteil (c)
     // 3 4 6 3 7 12 6
-    // Schema:
+    // Schema: a_n = 
     public static void folge_c() {
     	int n = 15;
     	int a = 3;
-    	int check = 0;
     	for (int i = 1; i <= n; i++) {
     		System.out.print(a + " ");
-    		switch (check) {
-    			case 0:
-    				a = a + i;
-    				check = 1;
-    				break;
-    			case 1:
-    				a = a + i;
-    				check = 2;
-    				break;
-    			case 2:
-    				a = a - i;
-    				check = 0;
-    				break;
-    		
-    		}
+    		a = (i%3 == 0) ? a - i : a + i;
     	
     	}
         
